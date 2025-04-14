55d2a6db35228081cde3a1532884c904e6354228
Andrezic <skywardflow@gmail.com>
2025-04-14 17:09:49 +0300

    Proiect Skyward Scraper complet sincronizat cu proxy Bright Data

--- utils/puppeteer.js
+++ utils/puppeteer.js
@@ -1,12 +1,10 @@
-const chromium = require('chrome-aws-lambda');
 const puppeteer = require('puppeteer-core');
 
 async function launchBrowser() {
   return await puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath,
-    headless: chromium.headless
+    executablePath: '/usr/bin/google-chrome',
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
 }
 
